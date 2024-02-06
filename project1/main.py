1a98ba8cf148426aa639b2f812e658bb3271afc5
WheatleyTheCore <alex.colwell@icloud.com>
2024-02-05 20:05:48 -0500

    recording now works

--- project1/main.py
+++ project1/main.py
@@ -1,5 +1,26 @@
-
+import sounddevice as sd
+from scipy.io.wavfile import write
+import matplotlib.pyplot as plt
+import librosa
 
 
 if __name__ == '__main__':
-    print("hello world!")
+    # Define Sampling Rate or Frequency in Hz
+    sr = 44100
+
+    # Record duration in seconds
+    duration = 5
+
+    recording = sd.rec(int(duration*sr), samplerate=sr, channels=2) # we will record with a  mono or stereo channel microphone
+
+    print("recording...............")
+    sd.wait()
+    write("sound.wav",sr,recording)
+
+
+    x, sr = librosa.load('sound.wav')
+    print("recording shape", x.shape)
+    print("sampling rate", sr)
+
+    plt.figure(figsize=(14, 5))
+    librosa.display.waveshow(x, sr=sr)  
